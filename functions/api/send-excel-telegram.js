6a5fe032540efc5ffac63ed7daf03aab76f9dd03
kuntheasom19-ship-it <kuntheasom19@gmail.com>
2026-08-19 13:35:46 +0700

    Auto-deploy update: functions/api/send-excel-telegram.js

--- functions/api/send-excel-telegram.js
+++ functions/api/send-excel-telegram.js
@@ -23,7 +23,7 @@ export async function onRequestPost(context) {
             } catch (e) {}
         }
         if (!token) {
-            token = '8853663191:AAFZXa_aYKGzNyVVwSmuX6V_dHYw6jbSy3M';
+            token = '8863517367:AAGiakygaWQASSwIJRhH52yidJA7GfZigZI';
         }
 
         // Convert Base64 string to Uint8Array Uint8Array / Blob
